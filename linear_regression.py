8f0c2bd8b7af331fd036fcf74e94755e7221f807
Hamza EL OTMANI <hamza.quarc@gmail.com>
2024-08-25 01:18:18 +0200

    Initial commit

---
+++ linear_regression.py
@@ -0,0 +1,59 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import datasets
+
+class LinearRegressionFromScratch:
+    def __init__(self, learning_rate=0.01, iterations=1000):
+        self.learning_rate = learning_rate
+        self.iterations = iterations
+        self.m = 0
+        self.c = 0
+
+    def predict(self, X):
+        return self.m * X + self.c
+
+    def cost_function(self, X, y):
+        N = len(y)
+        return (1/(2*N)) * np.sum((y - self.predict(X)) ** 2)
+
+    def gradient_descent(self, X, y):
+        N = len(y)
+        for i in range(self.iterations):
+            predictions = self.predict(X)
+            dm = (-2/N) * np.sum(X * (y - predictions))
+            dc = (-2/N) * np.sum(y - predictions)
+            self.m = self.m - self.learning_rate * dm
+            self.c = self.c - self.learning_rate * dc
+            if i % 100 == 0:  # Print cost every 100 iterations
+                cost = self.cost_function(X, y)
+                print(f"Iteration {i}: Cost {cost}, m {self.m}, c {self.c}")
+
+    def fit(self, X, y):
+        self.gradient_descent(X, y)
+
+    def plot(self, X, y):
+        predictions = self.predict(X)
+        plt.scatter(X, y, color='blue')
+        plt.plot(X, predictions, color='red')
+        plt.xlabel('Feature')
+        plt.ylabel('Target')
+        plt.title('Linear Regression Fit')
+        plt.show()
+
+
+# Create the dataset
+data = datasets.make_regression(n_samples=100, n_features=1, noise=20, random_state=4)
+X_data, y_data = data
+df = pd.DataFrame(X_data, columns=['Feature'])
+df['Target'] = y_data
+
+X = df.iloc[:, 0].values
+y = df.iloc[:, 1].values
+
+# Initialize and train the model
+model = LinearRegressionFromScratch(learning_rate=0.01, iterations=1000)
+model.fit(X, y)
+
+# Plot the results
+model.plot(X, y)
